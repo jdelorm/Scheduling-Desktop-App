05a7a56c74277ae3a288fcca499423e325495a17
jsdelorme30 <rockncourt30@aol.com>
2023-03-16 07:38:01 -0700

    save 1

--- src/main/java/delorme/john/Controllers/MainScreenController.java
+++ src/main/java/delorme/john/Controllers/MainScreenController.java
@@ -1,16 +1,38 @@
 package delorme.john.Controllers;
 
-import javafx.fxml.FXML;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class MainScreenController {
 
-    @FXML
-    private Label welcomeText;
+    public Button loginButtonMain;
+    public TextField userNameMain;
+    public TextField passwordMain;
+    public Label userLocationMain;
 
-    @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!!!!!!!");
+    public void onLoginButtonMain(ActionEvent actionEvent) throws IOException {
+
+        Parent root = FXMLLoader.load(getClass().getResource("/delorme/john/DirectoryScreen.fxml"));
+        Stage stage = (Stage)((Node) actionEvent.getSource()).getScene().getWindow();
+        Scene scene = new Scene(root, 600, 400);
+        stage.setTitle("Directory");
+        stage.setScene(scene);
+        stage.show();
+
     }
 
+    public void onUserNameMain(ActionEvent actionEvent) {
+    }
+
+    public void onPasswordMain(ActionEvent actionEvent) {
+    }
 }
