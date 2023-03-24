e0c1199ed5c998eaafdaf4193fab072c2c83a26b
jsdelorme30 <rockncourt30@aol.com>
2023-03-24 01:43:31 -0700

    save 1

---
+++ src/main/java/delorme/john/controllers/AppointmentAddScreenController.java
@@ -0,0 +1,66 @@
+package delorme.john.controllers;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.TextField;
+
+public class AppointmentAddScreenController {
+    public ComboBox appointmentScreenContactDropDown;
+    public DatePicker appointmentScreenEndDate;
+    public DatePicker appointmentScreenStartDate;
+    public TextField appointmentScreenUserID;
+    public TextField appointmentScreenCustomerID;
+    public TextField appointmentScreenType;
+    public Button appointmentScreenBackButton;
+    public Button appointmentScreenAddButton;
+    public ComboBox appointmentScreenEndTime;
+    public ComboBox appointmentScreenStartTime;
+    public TextField appointmentScreenLocation;
+    public TextField appointmentScreenDescription;
+    public TextField appointmentScreenTitle;
+    public TextField appointmentScreenAptID;
+
+    public void onAppointmentScreenAptID(ActionEvent actionEvent) {
+    }
+
+    public void onAppointmentScreenTitle(ActionEvent actionEvent) {
+    }
+
+    public void onAppointmentScreenDescription(ActionEvent actionEvent) {
+    }
+
+    public void onAppointmentScreenLocation(ActionEvent actionEvent) {
+    }
+
+    public void onAppointmentScreenStartTime(ActionEvent actionEvent) {
+    }
+
+    public void onAppointmentScreenEndTime(ActionEvent actionEvent) {
+    }
+
+    public void onAppointmentScreenAddButton(ActionEvent actionEvent) {
+    }
+
+    public void onAppointmentScreenBackButton(ActionEvent actionEvent) {
+    }
+
+    public void onAppointmentScreenType(ActionEvent actionEvent) {
+    }
+
+    public void onAppointmentScreenCustomerID(ActionEvent actionEvent) {
+    }
+
+    public void onAppointmentScreenUserID(ActionEvent actionEvent) {
+    }
+
+    public void onAppointmentScreenStartDate(ActionEvent actionEvent) {
+    }
+
+    public void onAppointmentScreenEndDate(ActionEvent actionEvent) {
+    }
+
+    public void onAppointmentScreenContactDropDown(ActionEvent actionEvent) {
+    }
+}
---
+++ src/main/java/delorme/john/controllers/CustomerUpdateScreenController.java
@@ -0,0 +1,45 @@
+package delorme.john.controllers;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextField;
+
+public class CustomerUpdateScreenController {
+    public Button customerScreenBackButton;
+    public Button customerScreenUpdateButton;
+    public ComboBox customerScreenCountryDropDown;
+    public ComboBox customerScreenStateDropDown;
+    public TextField customerScreenPhoneNumber;
+    public TextField customerScreenPostalCode;
+    public TextField customerScreenAddress;
+    public TextField customerScreenName;
+    public TextField customerScreenID;
+
+    public void onCustomerScreenID(ActionEvent actionEvent) {
+    }
+
+    public void onCustomerScreenName(ActionEvent actionEvent) {
+    }
+
+    public void onCustomerScreenAddress(ActionEvent actionEvent) {
+    }
+
+    public void onCustomerScreenPostalCode(ActionEvent actionEvent) {
+    }
+
+    public void onCustomerScreenPhoneNumber(ActionEvent actionEvent) {
+    }
+
+    public void onCustomerScreenStateDropDown(ActionEvent actionEvent) {
+    }
+
+    public void onCustomerScreenCountryDropDown(ActionEvent actionEvent) {
+    }
+
+    public void onCustomerScreenUpdateButton(ActionEvent actionEvent) {
+    }
+
+    public void onCustomerScreenBackButton(ActionEvent actionEvent) {
+    }
+}
